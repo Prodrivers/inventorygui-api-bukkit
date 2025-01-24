27b109f32ef2f68aae85284beb7b4ac1359e0961
Horgeon <me@horgeon.fr>
2025-01-24 18:25:55 +0100

    Fixed Mock implementation of InventoryView in regards to latest Spigot

--- src/main/java/me/eddie/inventoryguiapi/util/MockInventoryView.java
+++ src/main/java/me/eddie/inventoryguiapi/util/MockInventoryView.java
@@ -5,8 +5,10 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
-public class MockInventoryView extends InventoryView {
+public class MockInventoryView implements InventoryView {
 	private static Inventory INVENTORY = null;
 	private static final InventoryType TYPE = InventoryType.CHEST;
 
@@ -17,13 +19,13 @@ public class MockInventoryView extends InventoryView {
 	}
 
 	@Override
-	public Inventory getTopInventory() {
+	public @NonNull Inventory getTopInventory() {
 		init();
 		return INVENTORY;
 	}
 
 	@Override
-	public Inventory getBottomInventory() {
+	public @NonNull Inventory getBottomInventory() {
 		init();
 		return INVENTORY;
 	}
@@ -34,12 +36,66 @@ public class MockInventoryView extends InventoryView {
 	}
 
 	@Override
-	public InventoryType getType() {
+	public @NonNull InventoryType getType() {
 		return TYPE;
 	}
 
 	@Override
-	public String getTitle() {
+	public void setItem(int i, ItemStack itemStack) {
+		INVENTORY.setItem(i, itemStack);
+	}
+
+	@Override
+	public ItemStack getItem(int i) {
+		return INVENTORY.getItem(i);
+	}
+
+	@Override
+	public void setCursor(ItemStack itemStack) {}
+
+	@Override
+	public ItemStack getCursor() {
+		return null;
+	}
+
+	@Override
+	public Inventory getInventory(int i) {
+		return INVENTORY;
+	}
+
+	@Override
+	public int convertSlot(int i) {
+		return 0;
+	}
+
+	@Override
+	public InventoryType.@NonNull SlotType getSlotType(int i) {
+		return InventoryType.SlotType.CONTAINER;
+	}
+
+	@Override
+	public void close() {}
+
+	@Override
+	public int countSlots() {
+		return 0;
+	}
+
+	@Override
+	public boolean setProperty(@SuppressWarnings("removal") @NonNull Property property, int i) {
+		return false;
+	}
+
+	@Override
+	public @NonNull String getTitle() {
 		return getType().getDefaultTitle();
 	}
+
+	@Override
+	public @NonNull String getOriginalTitle() {
+		return getType().getDefaultTitle();
+	}
+
+	@Override
+	public void setTitle(@NonNull String s) {}
 }
